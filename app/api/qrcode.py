e327db69ec570f47a61cfcbf68f46c2be3c28e96
macOS <warmlab@me.com>
2019-05-05 18:57:49 +0800

    qrcode for promotion

--- app/api/qrcode.py
+++ app/api/qrcode.py
@@ -16,26 +16,35 @@ class QRCodeResource(BaseResource):
         parser.add_argument('X-PARTMENT', type=str, required=True, location='headers', help='partment code must be required')
         parser.add_argument('X-ACCESS-TOKEN', type=str, location='headers', required=True, help='access token must be required')
         parser.add_argument('path', type=str, required=True, help='program path must be required')
-        parser.add_argument('product', type=str, required=True, help='product code must be required')
+        parser.add_argument('id', type=int, required=True, help='product/promotion id  must be required')
+        parser.add_argument('type', type=str, required=True, help='type[promotion/product]  must be required')
         data = parser.parse_args()
-
-        print(data)
 
         shop = Shoppoint.query.filter_by(code=shopcode).first_or_404()
         partment = Partment.query.filter_by(shoppoint_id=shop.id, code=data['X-PARTMENT']).first_or_404()
         member = MemberOpenid.query.filter_by(shoppoint_id=shop.id, access_token=data['X-ACCESS-TOKEN']).first_or_404()
-        product = Product.query.filter_by(shoppoint_id=shop.id, code=data['product']).first_or_404()
+        if data['type'] == 'promotion':
+          target = Promotion.query.get_or_404(data['id'])
+          if tareget.shoppoint_id != shop.id:
+              logger.warning(MESSAGES[STATUS_NO_RESOURCE])
+              abort(404, status=STATUS_NO_RESOURCE, message=MESSAGES[STATUS_NO_RESOURCE])
+        else:
+          target = Product.query.get_or_404(data['id'])
+          if target.shoppoint_id != shop.id:
+              logger.warning(MESSAGES[STATUS_NO_RESOURCE])
+              abort(404, status=STATUS_NO_RESOURCE, message=MESSAGES[STATUS_NO_RESOURCE])
 
         #path = data['path'] + '?code='+data['product'] + '&user='+member.openid
         # print(path)
 
-        return self._generateQRCode(current_app.config['UPLOAD_FOLDER'], data['path'], partment, product, member)
+        return self._generateQRCode(current_app.config['UPLOAD_FOLDER'], data['path'], partment, product, member, data['type'])
 
-    def _generateQRCode(self, upload_folder, path, partment, product, user):
-        data = {
-            'path': path + '?code=' + product.code + '&member=' + user.openid,
-            'auto_color': True
-        }
+    def _generateQRCode(self, upload_folder, path, partment, target, user, type):
+        data = {'auto_color': True}
+        if type == 'promotion':
+          data['path'] = path + '?id=' + target.id + '&member=' + user.openid,
+        else:
+          data['path'] = path + '?code=' + target.code + '&member=' + user.openid,
         #url_param = urlencode(params).encode('utf-8')
         data = json.dumps(data).encode('utf-8')
         # with urlopen("https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=" + self.get_access_token(), data) as f:
